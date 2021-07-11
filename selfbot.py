9d7ddab3afe7d95e6a8aba4cea776a7c109f51c5
benji <87249724+rom4nce@users.noreply.github.com>
2021-07-10 17:24:24 -0700

    Create selfbot.py

---
+++ selfbot.py
@@ -0,0 +1,49 @@
+import discord, asyncio
+from os import system
+import shutil
+import subprocess
+
+client = discord.Client()
+
+token = ""
+
+def johnson(cmd):
+    subprocess.call(cmd, shell=True)
+
+@client.event
+async def on_ready():
+
+    width = shutil.get_terminal_size().columns
+
+    def ui():
+        print()
+        print("benji on top!".center(width))
+        print()
+        print("[-] made by benji [-]".center(width))
+        print("[-] user: {0} [-]".format(client.user).center(width))
+        print()
+    ui()
+ 
+ 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        commands = []
+        z = 0
+        for index, a in enumerate(message.content):
+            if a == " ":
+                commands.append(message.content[z:index])
+                z = index + 1
+        commands.append(message.content[z:])
+        channel = message.channel
+
+        if commands[0] == 'cl':
+                    if len(commands) == 1:
+                        async for msg in channel.history(limit=9999):
+                            if msg.author == client.user:
+                                try:
+                                    await msg.delete()
+                                except Exception as x:
+                                    pass
+
+client.run(token, bot=False)
